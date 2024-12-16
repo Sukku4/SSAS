5df1293b809054032a88b327f1cbf26e84a2faba
Sukku4 <sukumar.tems@gmail.com>
2024-12-16 16:36:30 +0530

    Update power_management.cpp

--- src/power_management.cpp
+++ src/power_management.cpp
@@ -2,11 +2,11 @@
 #include <esp_sleep.h>
 
 void setupPowerManagement() {
-  Serial.println("Power management initialized");
+    Serial.println("Power management initialized");
 }
 
 void enterDeepSleep(uint64_t timeInSeconds) {
-  Serial.printf("Entering deep sleep for %llu seconds...\n", timeInSeconds);
-  esp_sleep_enable_timer_wakeup(timeInSeconds * 1000000); // Convert to microseconds
-  esp_deep_sleep_start();
+    Serial.printf("Entering deep sleep for %llu seconds...\\n", timeInSeconds);
+    esp_sleep_enable_timer_wakeup(timeInSeconds * 1000000);
+    esp_deep_sleep_start();
 }
