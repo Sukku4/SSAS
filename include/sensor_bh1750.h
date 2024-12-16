608426beb9bf194f8fa51a0aa3b7b40e0b0bd4a2
Sukku4 <sukumar.tems@gmail.com>
2024-12-16 16:33:11 +0530

    Update sensor_bh1750.h

--- include/sensor_bh1750.h
+++ include/sensor_bh1750.h
@@ -1,7 +1,6 @@
 #ifndef SENSOR_BH1750_H
 #define SENSOR_BH1750_H
 
-void setupBH1750();
-uint16_t readLightIntensity();
-
-#endif // SENSOR_BH1750_H
+void initBH1750();
+float getLightIntensity();
+#endif
