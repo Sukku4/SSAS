39356f87717c6f0ea26d341e079a4883845b116e
Sukku4 <sukumar.tems@gmail.com>
2024-12-16 16:25:40 +0530

    Update comm_mqtt.cpp

--- src/comm_mqtt.cpp
+++ src/comm_mqtt.cpp
@@ -5,24 +5,37 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setupWiFi() {
-    delay(10);
-    WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-    }
+void setupMQTT(const char* broker) {
+  WiFi.begin("Your_SSID", "Your_PASSWORD");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  client.setServer(broker, 1883);
 }
 
-void reconnect() {
-    while (!client.connected()) {
-        if (client.connect("ESP32Client")) {
-            client.subscribe("inTopic");
-        } else {
-            delay(5000);
-        }
-    }
+void publishData(const char* topic, const String& payload) {
+  if (!client.connected()) {
+    reconnectMQTT();
+  }
+  client.loop();
+  client.publish(topic, payload.c_str(), false); // QoS 0 for reduced power
 }
 
-void sendToCloud(String payload) {
-    client.publish("outTopic", payload.c_str());
+String aggregateSensorData() {
+  String payload = "timestamp=2024-11-22T10:00:00Z,";
+  payload += "temperature=25.3,humidity=60.2,soilMoisture=45,";
+  payload += "lightIntensity=800.5,windSpeed=3.5,ndvi=0.85,co2=400";
+  return payload;
+}
+
+void reconnectMQTT() {
+  while (!client.connected()) {
+    if (client.connect("ESP32Client")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      delay(5000);
+    }
+  }
 }
