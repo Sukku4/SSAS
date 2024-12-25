9a16141a6a649aea3e57b27e0ce778bc5000a05e
Sukku4 <sukumar.tems@gmail.com>
2024-12-25 19:03:39 +0530

    Update comm_mqtt.cpp

--- src/comm_mqtt.cpp
+++ src/comm_mqtt.cpp
@@ -6,12 +6,6 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setupMQTT(const char* broker) {
-    WiFi.begin("Your_SSID", "Your_PASSWORD");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
-    Serial.println("Connected to WiFi");
     client.setServer(broker, 1883);
 }
 
@@ -19,22 +13,15 @@ void publishData(const char* topic, const String& payload) {
     if (!client.connected()) {
         reconnectMQTT();
     }
-    client.loop();
-    client.publish(topic, payload.c_str(), false);
-}
-
-String aggregateSensorData() {
-    String payload = "timestamp=2024-11-22T10:00:00Z,";
-    payload += "temperature=25.3,humidity=60.2,soilMoisture=45,";
-    payload += "lightIntensity=800.5,windSpeed=3.5,ndvi=0.85,co2=400";
-    return payload;
+    client.publish(topic, payload.c_str());
 }
 
 void reconnectMQTT() {
     while (!client.connected()) {
-        if (client.connect("ESP32Client")) {
-            Serial.println("Connected to MQTT broker");
+        if (client.connect("SSAS_Client")) {
+            Serial.println("Connected to MQTT Broker.");
         } else {
+            Serial.println("MQTT connection failed. Retrying...");
             delay(5000);
         }
     }
