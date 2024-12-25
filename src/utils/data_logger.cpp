5ef5549173d05f8e605ccddf6db9dfc4e2048ef2
Sukku4 <sukumar.tems@gmail.com>
2024-12-25 18:41:33 +0530

    Create data_logger.cpp

---
+++ src/utils/data_logger.cpp
@@ -0,0 +1,9 @@
+#include "utils/data_logger.h"
+
+void DataLogger::start() {
+    // Initialize logging mechanism (e.g., SD card, Serial)
+}
+
+void DataLogger::logData(const std::map<String, float>& data) {
+    // Log data to the chosen medium
+}
