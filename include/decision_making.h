1e219255c9482a8f535f564c24e45ab46e1d65a3
Sukku4 <sukumar.tems@gmail.com>
2024-12-25 19:07:05 +0530

    Create decision_making.h

---
+++ include/decision_making.h
@@ -0,0 +1,7 @@
+#ifndef DECISION_MAKING_H
+#define DECISION_MAKING_H
+
+void setupDecisionMaking();
+void processSensorData(float temperature, float humidity, float lightIntensity);
+
+#endif // DECISION_MAKING_H
